3a0a4b6ce92bec1000e445885d975ebea5df50c6
qqtk <qilong.cn@163.com>
2016-08-12 14:57:22 +0800

    Update xdrive_odom.cpp

--- src/xdrive_odom.cpp
+++ src/xdrive_odom.cpp
@@ -156,7 +156,12 @@ int main(int argc, char** argv)
         ROS_WARN("Not provided: d_diam. Default=0.262");
         d_diam  = 0.262;
     }
-
+    std::string odom_frame_id_;
+    if(!private_n->getParam("odom_frame", odom_frame_id_)) {
+        ROS_WARN("No odom_frame-str provided - default: odom");
+        odom_frame_id_ = "odom";
+    }
+    
     std::string imu_frame_id_;
     if(!private_n->getParam("imu_frame_id", imu_frame_id_)) {
         ROS_WARN("No imu_frame_id provided - default: quatIMU");
@@ -190,7 +195,7 @@ int main(int argc, char** argv)
   ros::Publisher ticksLR_pub = nh.advertise<robbase_msg::encoders>("/ticksLR", 20);
   // ros::Publisher ticksLR4_pub = nh.advertise<encoder_test::ticks>("/ticks", 20);
   //  ros::Publisher ticksMLR_pub = nh.advertise<robbase_msg::RazorImu>("/ticksMLR", 20);
-  ros::Publisher odom_pub = nh.advertise<nav_msgs::Odometry>("/odom", 20);
+  ros::Publisher odom_pub = nh.advertise<nav_msgs::Odometry>(odom_frame_id, 20);
 
   // showing motor_speed:
   cmd_vel_pub  = nh.advertise<robbase_msg::WheelSpeed>("/wheelspeed", 10);
